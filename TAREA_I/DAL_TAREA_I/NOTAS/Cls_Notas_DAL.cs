4221e13d3c50c7cee7b2c5ffc1b47a1e1673a874
Adrián Elizondo <adrian.elizondo1@uamcr.net>
2021-07-26 18:25:14 -0600

    index.html

---
+++ TAREA_I/DAL_TAREA_I/NOTAS/Cls_Notas_DAL.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL_TAREA_II.NOTAS
+{
+    public class Cls_Notas_DAL
+    {
+        #region Variables Privadas
+
+
+
+        #endregion
+
+        #region Constructores
+
+
+
+        #endregion
+    }
+}
